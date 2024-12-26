30a006b81d6b7ff4461a56323018ce34ec500f64
Adrián Moreno Peña <adrian.moreno.pena@sumup.com>
2024-12-26 13:51:12 +0100

    Added toggle logic

---
+++ assets/js/dark-toggle.js
@@ -0,0 +1,9 @@
+document.getElementById("dark-toggle").addEventListener("click", () => {
+    if (document.body.className.includes("dark")) {
+        document.body.classList.remove('dark');
+        localStorage.setItem("pref-theme", 'light');
+    } else {
+        document.body.classList.add('dark');
+        localStorage.setItem("pref-theme", 'dark');
+    }
+})
